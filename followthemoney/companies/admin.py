7ef7aafc052c365f0a7bf2e8af6921c74efdc5c7
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-07-13 16:16:16 -0700

    migrate tables to db and register with django admin

--- followthemoney/companies/admin.py
+++ followthemoney/companies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Rating, Classification, Company, Shareholder, Brand, Lawsuit
 # Register your models here.
+
+admin.site.register(Rating)
+admin.site.register(Classification)
+admin.site.register(Company)
+admin.site.register(Shareholder)
+admin.site.register(Brand)
+admin.site.register(Lawsuit)
