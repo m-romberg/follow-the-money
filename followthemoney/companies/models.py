9a441a1f3eb727ffd9b1050704bdbef841f0cde0
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-07-13 16:50:10 -0700

    complete table models
    added defaults for details column
    enforced nullability

--- followthemoney/companies/models.py
+++ followthemoney/companies/models.py
@@ -3,15 +3,18 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 ### Models for Company app
 
+DETAILS_DEFAULT = "Additional details currently not available."
+
 class Rating(models.Model):
     '''Ratings for companies, 1-10'''
     rating = models.IntegerField(
-        default=1,
+        default=5,
         validators=[
             MaxValueValidator(10),
             MinValueValidator(1)
         ]
      )
+    description =  models.TextField(default="")
 
     def __str__(self):
         return self.rating
@@ -31,10 +34,12 @@ class Company(models.Model):
     parent_company = models.ForeignKey("self",
                                        on_delete=models.CASCADE,
                                        related_name="parent")
-    logo = models.CharField(max_length=200)
+    logo = models.ImageField(max_length = 200)
     founded = models.DateField()
     num_employees = models.PositiveIntegerField()
     revenue = models.IntegerField()
+    details =  models.TextField(default=DETAILS_DEFAULT)
+    source = models.URLField(max_length = 200, blank=False, default=None)
 
     def __str__(self):
         return self.company
@@ -44,7 +49,9 @@ class Brand(models.Model):
     """Brand entry"""
     brand = models.CharField(max_length=50)
     parent_company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    logo = models.CharField(max_length=200)
+    logo = models.ImageField(max_length = 200)
+    details =  models.TextField(default=DETAILS_DEFAULT)
+    source = models.URLField(max_length = 200, blank=False, default=None)
 
     def __str__(self):
         return self.brand
@@ -56,8 +63,9 @@ class Shareholder(models.Model):
                                     related_name="shareholder")
     child_company = models.ForeignKey(Company,
                                       on_delete=models.CASCADE,
-                                      related_name="child_company")
-    source = models.CharField(max_length=200)
+                                      related_name="child")
+    details =  models.TextField(default=DETAILS_DEFAULT)
+    source = models.URLField(max_length = 200, blank=False, default=None)
 
     def __str__(self):
         return f"{self.shareholder}, {self.child_company}"
@@ -66,8 +74,8 @@ class Lawsuit(models.Model):
     """Lawsuits against companies"""
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     date = models.DateField()
-    details =  models.CharField(max_length=300)
-    source = models.CharField(max_length=200)
+    details =  models.TextField(default=DETAILS_DEFAULT)
+    source = models.URLField(max_length = 200, blank=False, default=None)
 
     def __str__(self):
         return f"{self.company}, {self.date}"
