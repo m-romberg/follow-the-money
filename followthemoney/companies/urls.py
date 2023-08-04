d40b45027ee4f455299f422d7b4d57c68d506908
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-08-04 14:53:57 -0700

    add placeholders for urls
    connected to urlsconfig

--- followthemoney/companies/urls.py
+++ followthemoney/companies/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #ex: /companies/
     path("", views.index, name="index"),
+    #ex: /companies/blackrock
+    path("<str:company>/", views.company, name="company"),
 ]
--- followthemoney/companies/views.py
+++ followthemoney/companies/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the companies index.")
+
+def company(request, company):
+    response = "You are looking at the results for company %s."
+    return HttpResponse(response % company)
